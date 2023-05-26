76473af60659829f88c1fa9c752809f090f3320a
Adam Mika <amika@squareup.com>
2023-05-26 08:35:21 -0600

    -n

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Web5 } from "@tbd54566975/web5";
 import { DidState } from "@tbd54566975/dids";
+import { ProfileApi } from "@tbd54566975/web5-user-agent";
 import "./App.css";
 
 // Using require statement, as there are problems importing ssi-sdk-wasm types
 const SSI = require("ssi-sdk-wasm");
 
-function App() {
-  const [didIon, setDidIon] = useState<DidState | undefined>(undefined);
-  const [didKey, setDidKey] = useState<any | undefined>(undefined);
+export const Web5Context = React.createContext(null);
 
-  async function createDidIon() {
-    setDidIon(await Web5.did.create("ion"));
-  }
+export default function App() {
+  const [didState, setDidState] = useState<DidState | undefined>(undefined);
+  const [vcs, setVcs] = useState<string[]>([]);
 
-  async function createDidKey() {
-    setDidKey(await SSI.createDIDKey());
-  }
+  useEffect(() => {
+    web5Connect();
+  }, []);
 
   async function web5Connect() {
-    const { web5, did } = await Web5.connect();
-    console.log("web5: " + JSON.stringify(web5));
-    console.log("connectedDid: " + did);
+    const { did } = await Web5.connect();
+
+    const profileApi = new ProfileApi();
+    const profile = await profileApi.getProfile(did);
+    setDidState(profile?.did);
+  }
+
+  async function selfSignNewVC() {
+    const privJwkKeyString = JSON.stringify(didState?.keys[0].privateKeyJwk);
+    var byteArray = new Uint8Array(new ArrayBuffer(privJwkKeyString.length));
+    for (var i = 0; i < privJwkKeyString.length; i++) {
+      byteArray[i] = privJwkKeyString.charCodeAt(i);
+    }
+
+    const result = await SSI.createVerifiableCredential(
+      didState?.id,
+      JSON.stringify(didState?.keys[0].privateKeyJwk),
+      JSON.stringify({ id: "blah", foo: "bar" })
+    );
+
+    setVcs((prev) => {
+      return [JSON.stringify(result), ...prev];
+    });
   }
 
   return (
     <div className="App">
-      <button onClick={createDidIon}>Create a did:ion with web5-js</button>
-      <p>{didIon ? didIon.id : ""}</p>
-      <button onClick={createDidKey}>Create a did:key with ssi-sdk-wasm</button>
-      <p>{didKey ? didKey.didDocument.id : ""}</p>
-      <button onClick={web5Connect}>Web5 Connect</button>
+      <p>didState: {didState ? JSON.stringify(didState) : "not created"}</p>
+      <h1>VCs</h1>
+      <ul>
+        {vcs.map((vc, index) => (
+          <li key={index}>{vc}</li>
+        ))}
+      </ul>
+      <button hidden={!didState} onClick={selfSignNewVC}>
+        Self Sign a New VC
+      </button>
     </div>
   );
 }
-
-export default App;
