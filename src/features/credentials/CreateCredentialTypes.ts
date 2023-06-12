b8c4f098369c629c640ff9184b9727cd6282511e
Adam Mika <amika@squareup.com>
2023-06-12 08:13:18 -0600

    -n

--- src/features/credentials/CreateCredentialTypes.ts
+++ src/features/credentials/CreateCredentialTypes.ts
@@ -9,33 +9,3 @@ type FormField = {
   name: string;
   label: string;
 };
-
-const warningSubtitle =
-  'PLEASE DO NOT INPUT REAL INFO! THIS IS JUST FOR TESTING!';
-
-export const CreateKycVcForm: CreateCredentialForm = {
-  title: 'KYC Verifiable Credential',
-  subtitle: warningSubtitle,
-  type: 'KYCAMLAttestation',
-  fields: [
-    { name: 'fullName', label: 'Full Name' },
-    { name: 'dob', label: 'Date of Birth' },
-  ],
-};
-
-export const CreateAchVcForm: CreateCredentialForm = {
-  title: 'ACH Verifiable Credential',
-  subtitle: warningSubtitle,
-  type: 'ACHAttestation',
-  fields: [
-    { name: 'accountNumber', label: 'Account Number' },
-    { name: 'routingNumber', label: 'Routing Number' },
-  ],
-};
-
-export const CreateBtcVcForm: CreateCredentialForm = {
-  title: 'Bitcoin Verifiable Credential',
-  subtitle: warningSubtitle,
-  type: 'BTCAttestation',
-  fields: [{ name: 'btcAddress', label: 'Bitcoin Wallet Address' }],
-};
